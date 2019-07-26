b7c89cfe9350c9b3f408d438d8aeb4bcf5aa9534
Federico Brigante <github@bfred.it>
2019-07-26 10:36:58 +0700

    Update webext-options-sync usage (#3)

--- source/options-storage.js
+++ source/options-storage.js
@@ -1,20 +1,13 @@
 import OptionsSync from 'webext-options-sync';
 
-const defaults = {
-	colorRed: 244,
-	colorGreen: 67,
-	colorBlue: 54
-};
-
-const options = new OptionsSync({
-	logging: true
-});
-
-options.define({
-	defaults,
+export default new OptionsSync({
+	defaults: {
+		colorRed: 244,
+		colorGreen: 67,
+		colorBlue: 54
+	},
 	migrations: [
 		OptionsSync.migrations.removeUnused
-	]
+	],
+	logging: true
 });
-
-export default options;
