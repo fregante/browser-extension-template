ca17fe444e5711a4d9f192a3822d875d430000f9
Federico Brigante <me@fregante.com>
2021-03-03 15:29:46 -0600

    Bundle webextension-polyfill in every file for now

--- source/options.js
+++ source/options.js
@@ -1,3 +1,6 @@
+// Don't forget to import this wherever you use it
+import browser from 'webextension-polyfill';
+
 import optionsStorage from './options-storage.js';
 
 optionsStorage.syncForm('#options-form');
