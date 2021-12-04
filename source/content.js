6e0e9216edf310714a60dfe4112409f4079ca0ff
Felipe Santos <40555737+felipelousantos@users.noreply.github.com>
2021-12-04 04:01:27 +0000

    Coding standard fixes

--- source/content.js
+++ source/content.js
@@ -1,17 +1,14 @@
 import optionsStorage from './options-storage.js';
 
-async function init () {
-
+async function init() {
 	const options = await optionsStorage.getAll();
-	const color = "rgb(" + options.colorRed + ", " + options.colorGreen + "," + options.colorBlue + ")";
+	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
 	const text = options.text;
-
-	const warning = document.createElement("div");
-
+	const warning = document.createElement('div');
 	warning.innerHTML = text;
-	document.body.appendChild(warning);
-	warning.id = "text-warning";
-	warning.style.border = "2px solid " + color;
+	document.body.append(warning);
+	warning.id = 'text-warning';
+	warning.style.border = '2px solid ' + color;
 	warning.style.color = color;
 }
 
