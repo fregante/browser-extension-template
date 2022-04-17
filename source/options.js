7f7bb733ee453032d2db4a390866c843e25aec3f
Federico Brigante <me@fregante.com>
2022-04-17 22:36:19 +0800

    CSS first

--- source/options.js
+++ source/options.js
@@ -1,8 +1,9 @@
-// Don't forget to import this wherever you use it
-import browser from 'webextension-polyfill';
 // eslint-disable-next-line import/no-unassigned-import
 import 'webext-base-css';
 import './options.css';
+
+// Don't forget to import this wherever you use it
+import browser from 'webextension-polyfill';
 
 import optionsStorage from './options-storage.js';
 
