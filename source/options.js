9640478f6af874566283acbb4089047c7a41dcc0
Domantas Petrauskas <domantas@zyro.com>
2022-04-17 12:26:34 +0300

    Update webext-base-css import path

--- source/options.js
+++ source/options.js
@@ -1,7 +1,7 @@
 // Don't forget to import this wherever you use it
 import browser from 'webextension-polyfill';
 
-import 'webext-base-css/webext-base.css';
+import 'webext-base-css';
 import './options.css';
 
 import optionsStorage from './options-storage.js';
