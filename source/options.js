5421956155ca460ee22142b99de1000d91019649
Domantas Petrauskas <domantas@zyro.com>
2022-04-17 16:56:36 +0300

    Disable eslint rule for webext-base-css import

--- source/options.js
+++ source/options.js
@@ -1,6 +1,6 @@
 // Don't forget to import this wherever you use it
 import browser from 'webextension-polyfill';
-
+// eslint-disable-next-line import/no-unassigned-import
 import 'webext-base-css';
 import './options.css';
 
