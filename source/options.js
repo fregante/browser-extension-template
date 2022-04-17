966ef1fb650fd0aef0bf5f471c241417a6ddd707
Domantas Petrauskas <domantas@zyro.com>
2022-04-17 17:37:04 +0300

    Add `webext-base-css` (#77)

--- source/options.js
+++ source/options.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line import/no-unassigned-import
+import 'webext-base-css';
+import './options.css';
+
 // Don't forget to import this wherever you use it
 import browser from 'webextension-polyfill';
 
