22ee6b6101623a68d4b590af0469e64a56295b9f
Federico Brigante <me@fregante.com>
2022-07-19 15:45:35 +0700

    Make demo content more visible

--- source/content.js
+++ source/content.js
@@ -1,12 +1,14 @@
+import browser from 'webextension-polyfill';
 import optionsStorage from './options-storage.js';
 
+console.log('💈 Content script loaded for', browser.runtime.getManifest().name);
 async function init() {
 	const options = await optionsStorage.getAll();
 	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
 	const text = options.text;
 	const notice = document.createElement('div');
 	notice.innerHTML = text;
-	document.body.append(notice);
+	document.body.prepend(notice);
 	notice.id = 'text-notice';
 	notice.style.border = '2px solid ' + color;
 	notice.style.color = color;
